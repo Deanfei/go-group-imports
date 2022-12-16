787649dc41fff96cae6d5eaf7479a0e13e015cd2
longfei <longfei.yang@lightspeedhq.com>
2022-12-16 23:24:30 +1300

    test

--- out/test/index.js
+++
@@ -1,7 +0,0 @@
-var testRunner = require('vscode/lib/testrunner');
-testRunner.configure({
-    ui: 'tdd',
-    useColors: true // colored output from test results
-});
-module.exports = testRunner;
-//# sourceMappingURL=index.js.map
